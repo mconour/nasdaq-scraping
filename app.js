7b25e0d55a236daa938a502cbe84e278c23e2ce6
Michael Conour <michaelconour@gmail.com>
2020-01-13 18:49:11 -0800

    edited app.js

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
+const app = express();
+app.use(cors());
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, err => {
+if (err) return console.log(err);
+console.log(`Server is running on Port ${port}`);
+})
