ebf96c988db048cb0dce5825234d63c955eb1353
Michael Conour <michaelconour@gmail.com>
2020-01-13 18:53:46 -0800

    edited app.js

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const app = express();
 app.use(cors());
 
 
+app.get('/', (req, res, next) => {
+    axios.get('https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download')
+    .then(response => {
+        let stockList = response.data.split('\r\n').map(stock => {
+            return stock.replace(/",/g, "").replace(/\"/g, "|").split("|").splice(1);
+        })
+        stockList.pop();
+        stockList.shift();
+    })
+
+});
+
+
 
 const port = process.env.PORT || 5000;
 
