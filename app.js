e4addc3e72771c503809f62ea08c95518d5837a3
Michael Conour <michaelconour@gmail.com>
2020-01-13 18:58:57 -0800

    added stockList map array method

--- app.js
+++ app.js
@@ -8,21 +8,31 @@ app.use(cors());
 
 app.get('/', (req, res, next) => {
     axios.get('https://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download')
-    .then(response => {
-        let stockList = response.data.split('\r\n').map(stock => {
-            return stock.replace(/",/g, "").replace(/\"/g, "|").split("|").splice(1);
+        .then(response => {
+            let stockList = response.data.split('\r\n').map(stock => {
+                return stock.replace(/",/g, "").replace(/\"/g, "|").split("|").splice(1);
+            })
+            stockList.pop();
+            stockList.shift();
+            let stocks = stockList.map(stock => {
+                return {
+                    symbol: stock[0],
+                    name: stock[1],
+                    lastSale: stock[2],
+                    marketCap: stock[3],
+                    ipoYear: stock[4],
+                    sector: stock[5],
+                    industry: stock[6],
+                    summaryQuote: stock[7]
+                }
+            })
+            return res.send(stocks)
         })
-        stockList.pop();
-        stockList.shift();
-    })
-
 });
-
-
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, err => {
-if (err) return console.log(err);
-console.log(`Server is running on Port ${port}`);
+    if (err) return console.log(err);
+    console.log(`Server is running on Port ${port}`);
 })
